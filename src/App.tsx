8c5de87bef7fd41084d9cc304f68b9a5e42f2bd2
NandaPersa <anandapersa@gmail.com>
2022-09-09 20:57:28 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const posts = [
+    {
+      title: 'Anandinha Sá',
+      resume: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repudiandae consequuntur maiores quaerat assumenda, enim ea ab reiciendis, dolor culpa doloribus explicabo consequatur vero voluptatibus ducimus eligendi? Provident, maiores vitae.',
+    },
+    {
+      title: 'Anandinha Sá 2',
+      resume: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repudiandae consequuntur maiores quaerat assumenda, enim ea ab reiciendis, dolor culpa doloribus explicabo consequatur vero voluptatibus ducimus eligendi? Provident, maiores vitae.',
+    },
+    {
+      title: 'Anandinha Sá 3',
+      resume: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repudiandae consequuntur maiores quaerat assumenda, enim ea ab reiciendis, dolor culpa doloribus explicabo consequatur vero voluptatibus ducimus eligendi? Provident, maiores vitae.',
+    },
+    {
+      title: 'Anandinha Sá 4',
+      resume: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repudiandae consequuntur maiores quaerat assumenda, enim ea ab reiciendis, dolor culpa doloribus explicabo consequatur vero voluptatibus ducimus eligendi? Provident, maiores vitae.',
+    },
+    {
+      title: 'Anandinha Sá 5',
+      resume: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure repudiandae consequuntur maiores quaerat assumenda, enim ea ab reiciendis, dolor culpa doloribus explicabo consequatur vero voluptatibus ducimus eligendi? Provident, maiores vitae.',
+    }
+  ]
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000); // 3000ms -> 3s
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      {posts.map((item) => (
+        <>
+         {loading ? <Skeleton circle={true} width={150} height={150} style={{ marginTop: '20px', marginBottom: '20px' }} /> : <img src="https://pbs.twimg.com/profile_images/1001169618441003009/y00d4GJO_400x400.jpg" width={150} height={150} style={{borderRadius: '75px'}} />}
+         {loading ? <Skeleton width="40%" height={60} style={{ marginTop: '20px', marginBottom: '20px' }}  /> : <h1>{item.title}</h1>}
+         {loading ? <Skeleton count={2} />  : <p>{item.resume}</p>}
+        </>
+      ))}
+   
     </div>
+    </>
   );
 }
 
